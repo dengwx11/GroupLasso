e041acc35114fbb90f5e9a2b6f718df4f960c9d2
dengwx11 <34903100+dengwx11@users.noreply.github.com>
2018-07-30 11:30:51 -0400

    7.30 at work

--- V4/sim4.R
+++ V4/sim4.R
@@ -1,5 +1,5 @@
 library(partitions)
-
+library(MASS)
 
 cov_block<-function(p,rho,block_num){
   block<-rmultinom(n = 1, size = p, prob = rep(1/block_num, block_num))
@@ -13,3 +13,42 @@ cov_block<-function(p,rho,block_num){
   return(corrmat)
 }
 
+
+
+sim_beta<-function(m_X,m_W,m_G,main_zero,inter_zero,bit=TRUE,hier=TRUE){
+  if(m_X!=0){
+    beta_X<-matrix(rnorm(m_X),m_X,1)
+  } else{ beta_X<-NULL}
+  if(m_W!=0){
+    beta_W<-matrix(rnorm(m_W),m_W,1)
+  } else{ beta_W<-NULL }
+  beta_G<-matrix(rnorm(m_G),m_G,1)
+  zero_main<-sample(1:m_G,main_zero,replace = F)
+  zero<-sample(1:m_G,inter_zero,replace = F)
+  beta_I<-matrix(rnorm(m_G),m_G,1)
+  
+  if(bit){
+    if(m_X!=0){
+      beta_X<-beta_X+sign(beta_X)*0.4
+    }
+    if(m_W!=0){
+      beta_W<-beta_W+sign(beta_W)*0.4
+    }
+    beta_G<-beta_G+sign(beta_G)*0.4
+    beta_I<-beta_I+sign(beta_I)*0.4
+  }
+  
+  if(hier){
+    beta_G[intersect(zero_main,zero)]<-0   
+  } else{
+    beta_G[zero_main]<-0
+  }
+  beta_I[zero]<-0   # Onl three are nonzero
+  
+  beta<-rbind(beta_X,beta_W,beta_G,beta_I)
+  return(beta)
+}
+
+
+sigma<-cov_block(100,.5,10)
+X<-mvrnorm(n=100,rep(0,100),sigma)
