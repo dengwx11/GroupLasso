31c1620cd6ea761f2251b77118072f5da9db0802
dengwx11 <34903100+dengwx11@users.noreply.github.com>
2018-08-01 16:40:29 -0400

    8.1 at work

--- V4/demo.R
+++ V4/demo.R
@@ -1,5 +1,17 @@
+n=200
+m_G<-200
 
-sigma<-cov_block(10,.5,3)
-X<-sim_X(1,10,sigma,100)
-beta<-sim_beta(0,1,10,5,6,T,F)
-y<-X%*%beta
+sigma<-cov_block(m_G,.5,7)
+x<-sim_X(1,m_G,sigma,n)
+beta<-sim_beta(0,1,m_G,floor(0.9*m_G),floor(0.9*m_G),T,F)
+y0<-x%*%beta
+
+SNR<-10
+
+noise<-rnorm(n,sd=1)
+SNRmtl <- as.numeric(sqrt(var(y0)/(SNR*var(noise))))
+y<-y0+SNRmtl*noise  
+colnames(x)<-c(1:dim(x)[2])
+truth<-which(beta!=0)
+a<-regsubsets(x=x,y=y,method="forward")
+summary(a)
