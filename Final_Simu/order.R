400d120b5eb7b4c2e15065da87d5c224284c0483
dengwx11 <34903100+dengwx11@users.noreply.github.com>
2018-08-13 17:42:54 -0400

    8.13 at work

---
+++ Final_Simu/order.R
@@ -0,0 +1,9 @@
+## Group Lasso Selection Order
+
+order<-function(X,y,f, gradf, g, proxg, x0, tau1, max_iters = 100, w = 10, 
+                backtrack = TRUE, recordIterates = FALSE, stepsizeShrink = 0.5, 
+                eps_n = 1e-15,m_X,m_W,m_G,m_I,lambda,lambda2,restart=TRUE){
+  
+  
+  
+}
